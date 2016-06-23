9a48b57cf5c28e1e2cf4735c45fde7e99186d116
FHedin <hedin.florent@gmail.com>
2016-06-23 13:53:52 +0200

    added db and scripts

---
+++ db/sqlite_create.sql
@@ -0,0 +1,100 @@
+PRAGMA synchronous = OFF;
+
+PRAGMA journal_mode = MEMORY;
+
+PRAGMA foreign_keys=ON;
+
+--
+-- Table structure for table `compounds`
+--
+
+CREATE TABLE IF NOT EXISTS "compounds" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+  "idPubchem" INTEGER NOT NULL,
+  "name" TEXT NOT NULL,
+  "added" DATE NOT NULL,
+  "lastUpdate" DATE NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ff`
+--
+
+CREATE TABLE IF NOT EXISTS "ff" (
+  "molID" INTEGER NOT NULL,
+  "parID" INTEGER DEFAULT NULL,
+  "topID" INTEGER DEFAULT NULL,
+  FOREIGN KEY(molID) REFERENCES compounds(id),
+  FOREIGN KEY(parID) REFERENCES par(id),
+  FOREIGN KEY(topID) REFERENCES top(id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `par`
+--
+
+CREATE TABLE IF NOT EXISTS "par" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+  "parFile" BLOB NOT NULL,
+  "description" TEXT NOT NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prop`
+--
+
+CREATE TABLE IF NOT EXISTS "prop" (
+  "id" INTEGER NOT NULL,
+  "mass" DOUBLE DEFAULT NULL,
+  "density" DOUBLE DEFAULT NULL,
+  "Hvap" DOUBLE DEFAULT NULL,
+  "Gsolv" DOUBLE DEFAULT NULL,
+  FOREIGN KEY(id) REFERENCES compounds(id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref`
+--
+
+CREATE TABLE IF NOT EXISTS "ref" (
+  "id" INTEGER NOT NULL,
+  "ref_dg" TEXT,
+  "ref_dh" TEXT,
+  FOREIGN KEY(id) REFERENCES compounds(id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `structure`
+--
+
+CREATE TABLE IF NOT EXISTS "structure" (
+  "id" INTEGER NOT NULL,
+  "formula" TEXT  NOT NULL,
+  "inchi" TEXT,
+  "smiles" TEXT NOT NULL,
+  FOREIGN KEY(id) REFERENCES compounds(id)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `top`
+--
+
+CREATE TABLE IF NOT EXISTS "top" (
+  "id" INTEGER PRIMARY KEY NOT NULL,
+  "topFile" BLOB NOT NULL,
+  "description" TEXT NOT NULL
+);
+
+
---
+++ scripts/charmm-ti/setup.py
@@ -0,0 +1,27 @@
+__author__="hedin"
+__date__ ="$Jun 9, 2015 3:58:26 PM$"
+
+from setuptools import setup,find_packages
+
+setup (
+  name = 'CharmmScripts',
+  version = '0.1',
+  packages = find_packages(),
+
+  # Declare your packages' dependencies here, for eg:
+  install_requires=['foo>=3'],
+
+  # Fill in these to make your Egg ready for upload to
+  # PyPI
+  author = 'hedin',
+  author_email = '',
+
+  summary = 'Just another Python package for the cheese shop',
+  url = '',
+  license = '',
+  long_description= 'Long description of the package',
+
+  # could also include long_description, download_url, classifiers, etc.
+
+  
+)
